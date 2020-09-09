e6b1358ee81b050614d41c67744da24e8a95dffb
Eliot Sanford <eliot587@gmail.com>
2020-09-09 06:09:25 -0500

    Build scaffolding for app features to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hosting-blog-map-app';
+  title = 'A Blog and Map App';
 }
